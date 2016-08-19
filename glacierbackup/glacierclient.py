7fcb2b5ba9b0b59f7feaf8e5ad8e7ec7afed2dcb
Daniel Poulin <dan.poulin@digitalsurgeons.com>
2016-08-19 17:20:30 -0400

    Gracefully handle missing credentials for glacier uploads

--- glacierbackup/glacierclient.py
+++ glacierbackup/glacierclient.py
@@ -4,12 +4,26 @@ import boto3
 class GlacierBackupGlacierClient:
 
     def __init__(self):
-        self.glacier_client = boto3.client('glacier')
+        try:
+            self.glacier_client = boto3.client('glacier')
+        except Exception as e:
+            self.glacier_client = None
+            print('warn: no credentials for upload to glacier')
 
     def upload(self, vault, file):
-        archive_meta = self.glacier_client.upload_archive(
-            vaultName=vault,
-            archiveDescription='Test',
-            body=file
-        )
-        print('Uploaded archive: ', archive_meta)
+        if self.glacier_client is not None:
+            archive_meta = self.glacier_client.upload_archive(
+                vaultName=vault,
+                archiveDescription='Test',
+                body=file
+            )
+
+            # Log successful upload to stdout
+            print(
+                'Uploaded archive ',
+                file,
+                ' with glacier id ',
+                archive_meta.archive_id,
+                ' to vault ',
+                vault
+            )
