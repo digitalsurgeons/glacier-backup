d191a236b666aec9c7f7f80a8a5626e0fd6932e6
Cory Zibell <cory.zibell@digitalsurgeons.com>
2016-11-09 12:02:15 -0500

    Fixed archive id metadata

--- glacier_backup/glacierclient.py
+++ glacier_backup/glacierclient.py
@@ -25,6 +25,6 @@ class GlacierBackupGlacierClient:
             self.logger.info(
                 'Uploaded archive %s with glacier id %s to vault %s',
                 file,
-                archive_meta.archive_id,
+                archive_meta.archiveId,
                 vault
             )
